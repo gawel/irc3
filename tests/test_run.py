37a70a9404eb1fd3911bb6aa29bdeefa7d06a02c
Gael Pasgrimaud <gael@gawel.org>
2014-01-04 19:55:46 +0100

    coverage

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 from docopt import DocoptExit
-from irc3 import run
+import tempfile
+import shutil
+import irc3
+import os
 
 
 class TestRun(TestCase):
 
+    def setUp(self):
+        self.addCleanup(setattr, irc3.IrcBot,
+                        'logging_config',
+                        irc3.config.LOGGING)
+        self.wd = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.wd)
+        self.wd = os.path.join(self.wd, 'logs')
+
     def callFTU(self, *args):
-        return run(args)
+        return irc3.run(args)
 
     def test_args_error(self):
         self.assertRaises(DocoptExit, self.callFTU, '-x')
@@ -15,3 +26,8 @@ class TestRun(TestCase):
     def test_configfile(self):
         bot = self.callFTU('-dr', 'examples/bot.ini')
         self.assertEqual(bot.config.autojoins, ['irc3'])
+
+    def test_logdir(self):
+        bot = self.callFTU('-dr', 'examples/bot.ini', '--logdir=' + self.wd)
+        self.assertEqual(
+            bot.logging_config['handlers']['console']['formatter'], 'file')
