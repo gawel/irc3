b555e47e45da59afe6d94bfecf9a3fb613f14729
Gael Pasgrimaud <gael@gawel.org>
2014-03-11 18:04:28 +0100

    Back to development: 0.4.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.4.6'
+version = '0.4.7.dev0'
 
 install_requires = ['venusian>=1.0a8', 'docopt']
 test_requires = [
