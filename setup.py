1c327e2a78effa556cd5c8d606ee8e03bf5c3d42
Gael Pasgrimaud <gael@gawel.org>
2015-11-04 22:05:34 +0100

    Back to development: 0.8.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import codecs
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.8.3'
+version = '0.8.4.dev0'
 
 install_requires = ['venusian>=1.0a8', 'docopt']
 test_requires = [
